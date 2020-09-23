c572aa8918cf983c764bb88261036f3ebbb7d632
contextCue <14878694+ed42311@users.noreply.github.com>
2020-09-23 13:49:29 -0600

    first commit

---
+++ index.ts
@@ -0,0 +1,32 @@
+// import * as pulumi from '@pulumi/pulumi'
+import * as awsx from '@pulumi/awsx'
+
+// Create a public HTTP endpoint (using AWS APIGateway)
+const endpoint = new awsx.apigateway.API('hello', {
+  routes: [
+    // Serve static files from the `www` folder (using AWS S3)
+    {
+      path: '/',
+      localPath: 'www',
+    },
+
+    // Serve a simple REST API on `GET /name` (using AWS Lambda)
+    {
+      path: '/source',
+      method: 'GET',
+      eventHandler: (req, ctx, cb) => {
+        cb(undefined, {
+          statusCode: 200,
+          body: Buffer.from(JSON.stringify({ name: 'AWS' }), 'utf8').toString(
+            'base64'
+          ),
+          isBase64Encoded: true,
+          headers: { 'content-type': 'application/json' },
+        })
+      },
+    },
+  ],
+})
+
+// Export the public URL for the HTTP service
+exports.url = endpoint.url
